1ce636ef9a3240b49ecc0e847367113d9c26adae
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2021-12-09 15:30:49 -0500

    commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app._static_folder = "static"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    data = request.form['projectFilepath']
+    print(data)
     return render_template('index.html')
 
 if (__name__ == "__main__"):
