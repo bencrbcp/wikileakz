e77f4b14450c199b4082a9debff0b6233fece135
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2021-12-09 16:30:12 -0500

    Working example

--- app.py
+++ app.py
@@ -6,16 +6,22 @@ app._static_folder = "static"
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = {}
-    start = request.form.get('start', '')
-    end = request.form.get('end', '')
-    data['start'] = start
-    data['end'] = end
-    # /wiki/
-    if start[0:6] == "/wiki/":
-        w = WikipediaNav(maxt=25)
-        res = w.searchAllFast(start, end)
-        data['res'] = res
     return render_template('index.html', data=data)
+
+@app.route('/handle_data', methods=['GET', 'POST'])
+def handle_data():
+        data = {}
+        start = request.form.get('start', '')
+        end = request.form.get('end', '')
+        data['start'] = start
+        data['end'] = end
+        # /wiki/
+        if start[0:6] == "/wiki/":
+            w = WikipediaNav(maxt=25)
+            res = w.searchAllFast(start, end)
+            data['res'] = res
+        return render_template('index.html', data=data)
+
 
 if (__name__ == "__main__"):
     app.run(debug=True)
